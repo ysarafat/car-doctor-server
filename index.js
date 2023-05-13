8fd616e8dddd6309c08e955576efc1df0562f001
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-05-13 11:45:11 +0600

    db connect

---
+++ index.js
@@ -0,0 +1,63 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+// mongodb start 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oxlnwju.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const servicesCollation = client.db('carDoctor').collection('services');
+
+    app.get('/services', async(req, res)=>{
+        const query = servicesCollation.find();
+        const result = await query.toArray()
+        res.send(result)
+    })
+
+
+    app.get('/service/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await servicesCollation.findOne(query);
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongodb end
+
+
+app.listen(PORT ,()=> {
+    console.log(`pognistooo running on ${PORT} port`);
+})
